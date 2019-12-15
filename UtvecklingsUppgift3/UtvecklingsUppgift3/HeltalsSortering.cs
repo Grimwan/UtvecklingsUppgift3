9e4fad1e83be19d3622f028ad44cb96184f18982
Unknown <deankulenovic@gmail.com>
2019-12-15 21:47:08 +0100

    Added quicksort and Sorting

---
+++ UtvecklingsUppgift3/UtvecklingsUppgift3/HeltalsSortering.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UtvecklingsUppgift3
+{
+   public class HeltalsSortering
+    {
+        private List<int> Container;
+
+        //the constructor creates the list and shuffles the data. 
+        public HeltalsSortering()
+        {
+            Container = new List<int>();
+            for(int i = 1;i<26;i++)
+            {
+                Container.Add(i);
+                Shuffle();
+                
+            }
+            QuickSort quick = new QuickSort(Container);
+        }
+        //shuffles the List, Utilises the Random class to Shuffle the List. 
+        public void Shuffle()
+        {
+            Random rng = new Random();
+            int n = Container.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                int value = Container[k];
+                Container[k] = Container[n];
+                Container[n] = value;
+            }
+        }
+
+    }
+}
---
+++ UtvecklingsUppgift3/UtvecklingsUppgift3/QuickSort.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtvecklingsUppgift3
+{
+    public class QuickSort
+    {
+        public QuickSort(List<int> list)
+        {
+            //size of the List
+            int Lowest = 0;
+            int Highest = 25;
+
+
+            //now the quicksort algorithm is working.
+            QuickSortFunction(list, Lowest, Highest);
+        }
+
+        public void QuickSortFunction(List<int> list, int LowestValue, int HighestValue)
+        {
+            if(LowestValue<HighestValue)
+            {
+                int Pivot_location = Partition(list, LowestValue, HighestValue);
+                QuickSortFunction(list, LowestValue, Pivot_location);
+                QuickSortFunction(list, Pivot_location + 1, HighestValue);
+            }
+        }
+
+        public int Partition(List<int> list, int LowestValue, int HighestValue)
+        {
+            int pivotlocation = LowestValue;
+            int pivot = list[LowestValue];
+            int leftwall = LowestValue;
+            for(int i = LowestValue+1;i<HighestValue;i++)
+            {
+                if( list[i]<pivot)
+                {
+                    int Storage = list[i];
+                    list[i] = list[leftwall];
+                    list[leftwall] = Storage;
+                    leftwall = leftwall + 1;
+                }
+            }
+            list[leftwall] = pivot;
+            
+
+
+            return leftwall;
+        }
+        
+    }
+}
